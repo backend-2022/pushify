b2fc3e082c7b876e15d198170720a3c1d326a39d
hassanmostfa <hassan.elshiat@gmail.com>
2026-04-27 14:39:24 +0300

    feat(routes): add web route group for Pushify endpoints

--- routes/pushify.php
+++ routes/pushify.php
@@ -3,11 +3,17 @@
 use Badawy\Pushify\Http\Controllers\PushifyController;
 use Illuminate\Support\Facades\Route;
 
+$registerRoutes = static function (): void {
+    Route::get('/', [PushifyController::class, 'index']);
+    Route::post('/', [PushifyController::class, 'store']);
+    Route::get('/{pushify}', [PushifyController::class, 'show']);
+    Route::post('/{pushify}/send', [PushifyController::class, 'send']);
+};
+
 Route::prefix(config('pushify.routes.prefix', 'pushify'))
     ->middleware(config('pushify.routes.middleware', ['api']))
-    ->group(function () {
-        Route::get('/', [PushifyController::class, 'index']);
-        Route::post('/', [PushifyController::class, 'store']);
-        Route::get('/{pushify}', [PushifyController::class, 'show']);
-        Route::post('/{pushify}/send', [PushifyController::class, 'send']);
-    });
+    ->group($registerRoutes);
+
+Route::prefix(config('pushify.routes.web_prefix', config('pushify.routes.prefix', 'pushify-web')))
+    ->middleware(config('pushify.routes.web_middleware', ['web']))
+    ->group($registerRoutes);
